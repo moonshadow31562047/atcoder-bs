3c3492b32262fb10c5b60ef173fc9409b722233f
ryuuichi <ap19090@azec.ac.jp>
2021-02-22 16:36:39 +0900

    abc088b card game for two

---
+++ abc088b.py
@@ -0,0 +1,14 @@
+N = int(input())
+cards = list(map(int,input().split()))
+cards.sort(reverse=True)
+
+alice = 0
+bob = 0
+
+for i in range(N):
+    if i % 2 == 0:
+        alice += cards[i]
+    else:
+        bob += cards[i]
+
+print(alice-bob)
